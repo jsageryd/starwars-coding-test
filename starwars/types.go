ae803dbdfbddfd91f314e118398bab0e0dd1a16c
Johan Sageryd <j@1616.se>
2023-06-13 16:28:08 +0200

    Clarify comment
    The SWAPI documentation hints that there's a space, but the actual data
    has no space.

--- starwars/types.go
+++ starwars/types.go
@@ -4,5 +4,5 @@ type Character struct {
 	Name      string  `json:"name"`
 	Height    float64 `json:"height,string,omitempty"` // height in cm
 	Mass      float64 `json:"mass,string,omitempty"`   // mass in kg
-	BirthYear string  `json:"birth_year,omitempty"`    // birth year, "<year> BBY" or "<year> ABY"
+	BirthYear string  `json:"birth_year,omitempty"`    // birth year, e.g. "<year>BBY" or "<year>ABY"
 }
